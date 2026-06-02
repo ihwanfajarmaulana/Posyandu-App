ce829577d048cecd9790ec05595709125ff7a107
KYY7976 <132982773+CmDevent31@users.noreply.github.com>
2026-06-03 00:33:08 +0700

    fix vite allowed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: [
+      'localhost',
+      'frontend-production-a882.up.railway.app'
+    ]
+  }
 })
