46b089ae8f1c7e7dc8a68a3cd2aabe8aa38d8926
KYY7976 <132982773+CmDevent31@users.noreply.github.com>
2026-06-03 01:15:22 +0700

    update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log('BASE URL:', import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
 });
